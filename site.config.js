bbfab8aad3db0760050159db8b9555c2890f0fdf
luwenlalala <36396400+luwenlalala@users.noreply.github.com>
2021-11-26 13:58:44 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd2746a6bd4064665a679465d396a92e0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'LW的个人主页',
+  domain: 'https://www.notion.so/notion-d2746a6bd4064665a679465d396a92e0',
+  author: 'LW',
 
   // open graph metadata (optional)
   description: 'Example site description',
